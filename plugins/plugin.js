0525fc4ff136eb04cab3586872eeb73db906dfd3
dan-caldwell <73503364+dan-caldwell@users.noreply.github.com>
2021-09-11 22:27:20 -0400

    SCSS -> HTML works
    also removed typescript.

--- plugins/plugin.js
+++ plugins/plugin.js
@@ -1,8 +1,8 @@
-import * as postcss from 'postcss';
-import util from 'util';
-import fs from 'fs';
-import sass, { render } from 'sass';
-import path from 'path';
+const postcss = require('postcss');
+const util = require('util');
+const fs = require('fs');
+const sass = require('sass');
+const path = require('path');
 
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
@@ -18,7 +18,7 @@ const getCssFromSass = async () => {
 
 
 class ZipperMerge {
-    inputPath: string = "../src";
+    inputPath = "../src";
 
     constructor() {
         this.init();
@@ -31,24 +31,99 @@ class ZipperMerge {
         const selectors = this.mapRules(rules);
         const renderedSass = await this.renderSass();
         const renderedSassObj = this.parseRenderedSass(renderedSass);
-        console.log(selectors);
+        const formattedSelectorArray = this.addNodesToSelectorsArray(selectors, renderedSassObj);
+        const tree = this.createTreeFromFlatArray(formattedSelectorArray);
+        const objTree = {
+            node: {
+                childNodes: tree
+            }
+        }
+        const output = this.traverse(objTree);
+        console.log(output);
+        //console.dir(tree, { depth: null});
+    }
+
+    traverse(obj) {
+        let contents = "\n";
+        if (obj.node.childNodes) {
+            for (const node of obj.node.childNodes) {
+                const text = this.getTextDeclaration(node.node)[0] || '';
+                contents += this.htmlFromSelector(node.selector, this.traverse(node), text);
+            }
+        }
+        return contents;
+    }
+
+    getTextDeclaration = (node) =>
+        node.nodes
+            .filter(node => node.type === "decl" && node.prop === "text")
+            .reverse().map((decl) => {
+                const firstVal = decl.value.charAt(0);
+                const lastVal = decl.value.charAt(decl.value.length - 1);
+                if (firstVal === "'" || firstVal === '"') decl.value = decl.value.slice(1);
+                if (lastVal === "'" || lastVal === '"') decl.value = decl.value.slice(0, decl.value.length - 1);
+                return decl.value;
+            });
+
+    createTreeFromFlatArray(dataset) {
+        const hashTable = Object.create(null);
+        dataset.forEach(aData => {
+            hashTable[aData.renderedSelector] = { ...aData };
+            hashTable[aData.renderedSelector].node.childNodes = [];
+            delete hashTable[aData.renderedSelector].node.parent;
+            delete hashTable[aData.renderedSelector].node.source;
+        });
+        const dataTree = [];
+        dataset.forEach(aData => {
+            if (aData.parent) {
+                hashTable[aData.parent].node.childNodes.push(hashTable[aData.renderedSelector]);
+            } else {
+                dataTree.push(hashTable[aData.renderedSelector]);
+            }
+        });
+        return dataTree;
+    }
+
+    htmlFromSelector(selector, traversal, innerText = "") {
+        if (selector.startsWith('#') || selector.startsWith('.')) selector = 'div' + selector;
+        const arr = [
+            [/#([\w-]+)/, ` id="$1"`],
+            [/((\.[\w-]+)+)/, (_, c) => ` class="${c.split`.`.join` `.trim()}"`],
+            [/(\[.+?\])/g, (_, a) => " " + a.slice(1, -1)],
+            [/([\S]+)(.*)/, `<$1$2>${innerText}${traversal}</$1>`]
+        ].map((replacement) => {
+            const regex = replacement[0];
+            const str = replacement[1];
+            selector = selector.replace(regex, str);
+            return selector;
+        }
+        )[3];
+        return arr;
+    }
+
+    addNodesToSelectorsArray(selectorsArray, nodesArray) {
+        return selectorsArray.map(selector => {
+            const foundNode = nodesArray.find(node => node.selector === selector.renderedSelector);
+            selector.node = foundNode;
+            return selector;
+        }).filter(selector => selector.node);
     }
 
     // Map rules to selectors to easily traverse
-    mapRules(rules: any[]) {
+    mapRules(rules) {
         return rules.map(rule => {
-            const { renderedSelector, selector } = rule;
-            //console.log(rule);
+            const { renderedSelector, selector, parentId } = rule;
+            const children = rule.nodes.filter((node) => node.type === "rule").map((child) => child.renderedSelector);
             return {
-                renderedSelector, selector
+                renderedSelector, selector, parent: parentId || null, children
             }
         });
     }
 
     // Parse rendered Sass
-    parseRenderedSass(contents: string) {
-        const data: postcss.Root = postcss.parse(contents, {});
-        return data;
+    parseRenderedSass(contents) {
+        const data = postcss.parse(contents, {});
+        return data.nodes || [];
     }
 
     // Render SCSS file
@@ -61,8 +136,8 @@ class ZipperMerge {
     }
 
     // Gets all file paths in a directory
-    async getAllFilePaths(dirPath: string, arrayOfFiles: string[] | null): Promise<string[]> {
-        let files: string[] = [];
+    async getAllFilePaths(dirPath, arrayOfFiles) {
+        let files = [];
         try {
             files = await readdir(dirPath);
         } catch (err) {
@@ -86,12 +161,12 @@ class ZipperMerge {
     }
 
     // Read raw SCSS file paths and create an output object based on files
-    async createObjFromFiles(filePaths: string[]) {
-        let dataObjects: postcss.ChildNode[] = [];
+    async createObjFromFiles(filePaths) {
+        let dataObjects = [];
         for (const filePath of filePaths) {
             try {
                 const contents = await readFile(filePath);
-                const data: postcss.Root = postcss.parse(contents, {});
+                const data = postcss.parse(contents, {});
                 const rules = data.nodes.filter(node => node.type === "rule");
                 if (rules.length) dataObjects = dataObjects.concat(rules);
             } catch (err) {
@@ -101,15 +176,15 @@ class ZipperMerge {
         return Object.assign(dataObjects);
     }
 
-    getListOfAllRules(sassObj: any) {
-        let output: any[] = [];
+    getListOfAllRules(sassObj) {
+        let output = [];
         for (const rule of sassObj) {
-            const filteredRules = rule.nodes.filter((node: postcss.Rule) => node.type === "rule");
-            filteredRules.forEach((filteredRule: any) => {
+            const filteredRules = rule.nodes.filter((node) => node.type === "rule");
+            filteredRules.forEach((filteredRule) => {
                 this.getParent(filteredRule);
                 filteredRule.renderedSelector = filteredRule.parentId + " " + filteredRule.selector;
             });
-            output = output.concat(this.getListOfAllRules(filteredRules as postcss.Rule[]));
+            output = output.concat(this.getListOfAllRules(filteredRules));
             // Get the parent and renderedSelector
             this.getParent(rule);
             rule.renderedSelector = rule.parentId ? rule.parentId + " " + rule.selector : rule.selector;
@@ -132,14 +207,14 @@ class ZipperMerge {
     }
 
     // Get list of selectors from CSS rules
-    getSelectors(obj: postcss.Rule | postcss.Root, outputType: 'html' | 'css') {
+    getSelectors(obj, outputType) {
         let contents = "\n";
         if (obj.nodes) {
             for (const node of obj.nodes) {
                 if (node.type === "rule") {
-                    const nodeRef: any = node;
+                    const nodeRef = node;
                     if (node.parent?.type === "rule") {
-                        const parentNode: any = node.parent;
+                        const parentNode = node.parent;
                         const parent = parentNode.selector || "";
                         nodeRef.parentId = parentNode.parentId ? parentNode.parentId + ' ' + parent : parent;
                     }
@@ -152,14 +227,14 @@ class ZipperMerge {
         return contents;
     }
 
-    getParent(node: any) {
+    getParent(node) {
         if (node.parent?.type === "rule") {
-            const parentNode: any = node.parent;
+            const parentNode = node.parent;
             const parent = parentNode.selector || "";
             node.parentId = parentNode.parentId ? parentNode.parentId + ' ' + parent : parent;
         }
     }
-    
+
 }
 
 new ZipperMerge();
@@ -187,47 +262,47 @@ body {
 `;
 
 // Create HTML based on the selector
-const htmlFromSelector = (selector: string, traversal: string, innerText: string = "") => {
+const htmlFromSelector = (selector, traversal, innerText = "") => {
     if (selector.startsWith('#') || selector.startsWith('.')) selector = 'div' + selector;
     const arr = [
-        [/#([\w-]+)/,` id="$1"`],
-        [/((\.[\w-]+)+)/,(_: string, c: any)=>` class="${c.split`.`.join` `.trim()}"`],
-        [/(\[.+?\])/g,(_: string, a: any)=>" "+a.slice(1,-1)],
-        [/([\S]+)(.*)/,`<$1$2>${innerText}${traversal}</$1>`]
-    ].map((replacement: any[]) => {
-        const regex: RegExp = replacement[0];
-        const str: string = replacement[1];
+        [/#([\w-]+)/, ` id="$1"`],
+        [/((\.[\w-]+)+)/, (_, c) => ` class="${c.split`.`.join` `.trim()}"`],
+        [/(\[.+?\])/g, (_, a) => " " + a.slice(1, -1)],
+        [/([\S]+)(.*)/, `<$1$2>${innerText}${traversal}</$1>`]
+    ].map((replacement) => {
+        const regex = replacement[0];
+        const str = replacement[1];
         selector = selector.replace(regex, str);
         return selector;
     }
     )[3];
     return arr;
 }
-const root: postcss.Root = postcss.parse(css, {});
+const root = postcss.parse(css, {});
 
 // Get the styles of the declaration
-const getStyles = (node: postcss.Rule, filterInvalidDecls: boolean): string => 
+const getStyles = (node, filterInvalidDecls) =>
     node.nodes
-    .filter((node: any) => {
-        if (filterInvalidDecls && node.type === "decl") {
-            // List of declarations to find
-            const filterProps = [
-                { key: 'text', exact: true }, 
-                { key: ']', exact: false }
-            ];
-            const inFilter = filterProps.find(item => item.exact ? item.key === node.prop : node.prop.includes(item.key));
-            return !inFilter;
-        } else return node.type === "decl";
-    }) // Only include declarations, exclude text declarations
-    .map(decl => decl.toString() + ";") // Convert declarations to CSS strings
-    .join('\n\t'); // Join declarations to string
+        .filter((node) => {
+            if (filterInvalidDecls && node.type === "decl") {
+                // List of declarations to find
+                const filterProps = [
+                    { key: 'text', exact: true },
+                    { key: ']', exact: false }
+                ];
+                const inFilter = filterProps.find(item => item.exact ? item.key === node.prop : node.prop.includes(item.key));
+                return !inFilter;
+            } else return node.type === "decl";
+        }) // Only include declarations, exclude text declarations
+        .map(decl => decl.toString() + ";") // Convert declarations to CSS strings
+        .join('\n\t'); // Join declarations to string
 
-const getTextDeclaration = (node: postcss.Rule) => 
+const getTextDeclaration = (node) =>
     node.nodes
-    .filter(node => node.type === "decl" && node.prop === "text")
-    .reverse().map((decl: any) => decl.value);
+        .filter(node => node.type === "decl" && node.prop === "text")
+        .reverse().map((decl) => decl.value);
 
-const traverse = (obj: postcss.Rule | postcss.Root, outputType: 'html' | 'css') => {
+const traverse = (obj, outputType) => {
     let contents = "\n";
     if (obj.nodes) {
         for (const node of obj.nodes) {
@@ -240,9 +315,9 @@ const traverse = (obj: postcss.Rule | postcss.Root, outputType: 'html' | 'css') 
                     break;
                 case 'css':
                     if (node.type === "rule") {
-                        const nodeRef: any = node;
+                        const nodeRef = node;
                         if (node.parent?.type === "rule") {
-                            const parentNode: any = node.parent;
+                            const parentNode = node.parent;
                             const parent = parentNode.selector || "";
                             nodeRef.parentId = parentNode.parentId ? parentNode.parentId + ' ' + parent : parent;
                         }
